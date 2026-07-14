49aa2aa0edd20b9017d89a81ecacd660427f7de2
tenmushi <tentoumushi0319@icloud.com>
2026-07-14 16:47:46 +0900

    scene分割までやった中身はまだない

--- Assets/Scripts/NetworkBootstrap.cs
+++ Assets/Scripts/NetworkBootstrap.cs
@@ -3,26 +3,55 @@ using UnityEngine;
 
 public class NetworkBootstrap : MonoBehaviour
 {
+    private string joinCodeInput = "";
+    private string myJoinCode = "";
+    private RelayManager relayManager;
+
+    void Start()
+    {
+        relayManager = GetComponent<RelayManager>();
+    }
+
     void OnGUI()
     {
-        // NetworkManagerがまだ準備できていない場合は何もしない
-        if (NetworkManager.Singleton == null)
-        {
-            return;
-        }
+        if (NetworkManager.Singleton == null) return;
 
-        GUILayout.BeginArea(new Rect(10, 10, 200, 150));
+        GUILayout.BeginArea(new Rect(10, 10, 300, 200));
 
         if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer)
         {
-            if (GUILayout.Button("Host")) NetworkManager.Singleton.StartHost();
-            if (GUILayout.Button("Client")) NetworkManager.Singleton.StartClient();
+            if (GUILayout.Button("部屋を作る (Host)"))
+            {
+                CreateRoom();
+            }
+
+            GUILayout.Space(10);
+
+            joinCodeInput = GUILayout.TextField(joinCodeInput);
+            if (GUILayout.Button("コードで参加 (Client)"))
+            {
+                JoinRoom();
+            }
         }
         else
         {
+            if (!string.IsNullOrEmpty(myJoinCode))
+            {
+                GUILayout.Label("参加コード: " + myJoinCode);
+            }
             GUILayout.Label(NetworkManager.Singleton.IsHost ? "Host起動中" : "Client起動中");
         }
 
         GUILayout.EndArea();
+    }
+
+    private async void CreateRoom()
+    {
+        myJoinCode = await relayManager.CreateRelay();
+    }
+
+    private async void JoinRoom()
+    {
+        await relayManager.JoinRelay(joinCodeInput);
     }
 }
